6e00113ed18b6e25ea3d894210df4ca3e9bcf4ca
aguidor <157438836+aguidor@users.noreply.github.com>
2024-03-15 14:28:24 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -11,3 +11,21 @@ while usuario != "admin" or clave != "123":
         print("¡Bienvenido!")
     else:
         print("Acceso denegado, inténtelo otra vez")
+
+
+#Sistema informacion clientes
+clientes = int(input("Ingesar el numero de clientes: "))
+
+for i in range(clientes):
+    nombre = input("Ingresar nombre del cliente:")
+    correo = input("Ingresar correo electronico:")
+    direccion = input("Ingresar direccion:")
+    telefono = int(input("Numero de telefono:"))
+
+    nombres.append(nombre)
+    correos.append(correo)
+    direcciones.append(direccion)
+    telefonos.append(telefono)
+for i in range(clientes):
+    print(f"--NOMBRE: {nombres[i]}")
+    print(f"      --Correo: {correos[i]}  --Direccion: {direcciones[i]}/ Telefono: {telefonos[i]}")
